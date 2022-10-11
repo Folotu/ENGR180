5386369b2a99a30201a405887f3a7d97161627e9
Frank Olotu <61606335+Folotu@users.noreply.github.com>
2022-10-10 22:00:40 -0700

    Add files via upload

---
+++ Lab3-2/Part2b-1Script.py
@@ -0,0 +1,14 @@
+﻿# coding: utf-8
+print("HelloWorld")
+# HelloWorld
+import arcpy
+arcpy.env.workspace = "C:\\Users\\Frank\\Documents\\UCM\\ENGR180\\Lab3-1\\Lab3-1\\Lab3-1.gdb"
+in_features = "CARiversAndCreeksPRJ"
+clip_features = "MariposaCountyBoundary"
+out_feature_class = "MarCoRiversAndCreeks_PY"
+arcpy.Clip_analysis(in_features, clip_features, out_feature_class)
+# <Result 'C:\\Users\\Frank\\Documents\\UCM\\ENGR180\\Lab3-1\\Lab3-1\\Lab3-1.gdb\\MarCoRiversAndCreeks_PY'>
+
+import arcpy 
+arcpy.env.workspace = "C:\\Users\\Frank\\Documents\\UCM\\ENGR180\\Lab3-1\\Lab3-1\\Lab3-1.gdb" 
+string = "Hello World"
---
+++ Lab3-2/Part4a-Script.py
@@ -0,0 +1,18 @@
+﻿# coding: utf-8
+import arcpy
+
+arcpy.env.workspace ="C:\\Users\\Frank\\Documents\\UCM\\ENGR180\\Lab3-1\\Lab3-1\\Lab3-1.gdb"
+in_raster= "DiffNBR_ModBuild"
+
+reclass_field = "VALUE"
+
+remap = RemapRange([[-0.727323,-0.500000, 1], 
+                    [-0.500000, -0.250000, 2], 
+                    [-0.25, -0.1, 3], 
+                    [-0.1, 0, 4], 
+                    [0, 0.1, 5], 
+                    [0.1, 0.25, 6], 
+                    [0.25, 0.5, 7], 
+                    [0.5, 0.89, 8]])
+
+reclass_NBR= arcpy.sa.Reclassify(in_raster, reclass_field, remap, 'NODATA')
